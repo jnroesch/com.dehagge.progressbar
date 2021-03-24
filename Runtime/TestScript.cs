ca8f76c1e75312e2a7a8cabe8a842808a3cbbb28
Jan Niklas Roesch <j.n.roesch@googlemail.com>
2021-03-25 00:09:27 +0100

    Initial package upload

---
+++ Runtime/TestScript.cs
@@ -0,0 +1,15 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Packages.com.dehagge.progressbar.Runtime
+{
+    public class TestScript : MonoBehaviour
+    {
+        private Image _image;
+        private void Start()
+        {
+            _image.DOFade(0, 3);
+        }
+    }
+}
